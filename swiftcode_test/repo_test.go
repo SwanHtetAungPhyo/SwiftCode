27c1e964789c44ab512916f8d80fcd06cd0732f5
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2025-03-28 00:23:44 +0100

    Comment Test Case

--- swiftcode_test/repo_test.go
+++ swiftcode_test/repo_test.go
@@ -73,7 +73,7 @@ func TestInsertion(t *testing.T) {
 	}
 	log := logrus.New()
 	processor := services.NewBankProcessor(instance, log)
-	processor.ProcessData("data/swift_codes.csv")
+	processor.ProcessData("../data/swift_codes.csv")
 	err = truncateTablesAfterTest(instance)
 	if err != nil {
 		log.Error(err.Error())
